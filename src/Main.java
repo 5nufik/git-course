e26c9c868e06f8f6646eee7e3928e55b3e943fac
Max Nikiforov <max.nikifo@yandex.ru>
2021-06-23 06:09:36 +0300

    Тест 6

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        Bird bird = new Bird();
-
-        dog.say();
-        cat.say();
-        bird.say();
-
-        System.out.println("Hello git");
-        System.out.println("Test1");
-        System.out.println("Test2");
+        System.out.println("Hi Ivan");
     }
 }
